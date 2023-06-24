27414f3339fa7f69b8fb6df089b6f48ae82761bf
yousuf <yosefmahmoud356@gmail.com>
2023-06-24 16:52:01 +0300

    .

---
+++ simplekivy/simplekivy.py
@@ -0,0 +1,42 @@
+import kivy
+from kivy.factory import Factory as F
+
+from kivy.clock import mainthread
+
+from threading import Thread
+from time import sleep
+
+from app import MainApp
+
+
+class SimpleKivy:
+    def __init__(self, *args, **kwargs):
+        self.app = MainApp(*args, **kwargs)
+        
+    #use + to add widgets
+    def __add__(self, widgets):
+        self.app.widgets = widgets
+        self.app.run()        
+         
+
+    def __getattr__(self, attr):
+        if hasattr(kivy, attr):
+            return getattr(kivy, attr)
+
+        elif hasattr(F, attr):
+            return getattr(F, attr)
+
+        else:
+            raise Exception(attr + " doasn't exists")
+
+
+
+
+if __name__ == "__main__":
+    s = SimpleKivy(title = "gsg")
+
+    x = s.Label(text = "dssds")
+    s + [
+        s.Button(text="gdg", pos_hint={"center_x":.5}, size_hint=(.4, .1)),
+        s.Button(text="gdgdd", size_hint=(.4, .1))
+    ]
