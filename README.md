e0baa7bfeb2a581201d78423ab65a3cd9823b4e5
yousuf <yosefmahmoud356@gmail.com>
2023-06-26 16:52:54 +0300

    .

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 ```python
 from simplekivy import SimpleKivy
 
-s = SimpleKivy(title = "test")
-s + [
-    s.Label()
+s + [[
+    s.Button(text="gdddg"),
+    s.Button(text="gdgdd")],
+    (
+    s.Label(text="ffljwfe", pos_hint={"center_x":.1, "center_y":.5}),
+    s.Label(text="ffljwfe", pos_hint={ "center_y":.5}),),
+    [s.Button(text="third button"), s.Label(text="third button")],
+    [s.Button(text="third button"), s.Button(text="third button")],
 ]
 ```
--- simplekivy/simplekivy.py
+++ simplekivy/simplekivy.py
@@ -31,16 +31,3 @@ class SimpleKivy:
 
 
 
-
-if __name__ == "__main__":
-    s = SimpleKivy(title = "gsg")
-    
-    s + [[
-        s.Button(text="gdddg"),
-        s.Button(text="gdgdd")],
-        (
-        s.Label(text="ffljwfe", pos_hint={"center_x":.1, "center_y":.5}),
-        s.Label(text="ffljwfe", pos_hint={ "center_y":.5}),),
-        [s.Button(text="third button"), s.Label(text="third button")],
-        [s.Button(text="third button"), s.Button(text="third button")],
-    ]
