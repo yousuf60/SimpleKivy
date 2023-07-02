80efc1d022f5808dc8a9040177a7d94c034b4dc1
yousuf <yosefmahmoud356@gmail.com>
2023-07-02 19:47:52 +0300

    that allows adding list inside tuple ets...

--- simplekivy/widgets/app.py
+++ simplekivy/widgets/app.py
@@ -5,24 +5,32 @@ from kivy.lang import Builder
 import os.path
 
 file_path = os.path.abspath(os.path.dirname(__file__))
+print(file_path)
 Builder.load_file(os.path.join(file_path, "main.kv"))
 
 class MainApp(App):
     widgets = []
     def on_start(self):
+        
         for widofmainlist in self.widgets:
-            #list for boxlayout
-            if type(widofmainlist) is list:
-                parent = F.BoxLayout(orientation="horizontal")
-                self.row_widget_adder(widofmainlist, parent)
-
-            #tuple for floatlayout
-            elif type(widofmainlist) is tuple:
-                parent = F.FloatLayout()
-                self.row_widget_adder(widofmainlist, parent) 
-
-            else:    
+            if type(widofmainlist) in (list, tuple):
+                self.freeze(widofmainlist)
+            else:
                 self.add_to_root(widofmainlist)
+
+
+    def freeze(self, wid):
+        #list for boxlayout
+        if type(wid) is list:
+            parent = F.BoxLayout(orientation="horizontal")
+            self.row_widget_adder(wid, parent)
+
+        #tuple for floatlayout
+        elif type(wid) is tuple:
+            parent = F.FloatLayout()
+            self.row_widget_adder(wid, parent) 
+
+            
 
     def add_to_root(self, widget):
         self.add_to_widget(widget, self.root)
@@ -35,11 +43,14 @@ class MainApp(App):
             parent.add_widget(widget) 
 
     def row_widget_adder(self, widofmainlist, parent):
+        
         for item in widofmainlist:
+            print(item)
             #put new boxlayout or floatlayout kwargs
             if type(item) is dict:
-                print(item)
                 parent.__init__(**item)
-                
+                print("dict")
+            elif type(item) in (list, tuple):
+                self.freeze(item)
             else:self.add_to_widget(item, parent)
         self.add_to_root(parent)        
