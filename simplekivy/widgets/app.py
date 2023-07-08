942abccbf8179efb21f0a4f3abc521f52df136d0
yousuf <yosefmahmoud356@gmail.com>
2023-07-09 02:30:00 +0300

    fixed the widgets adding and the widgets tree becomes  more since

--- simplekivy/widgets/app.py
+++ simplekivy/widgets/app.py
@@ -13,8 +13,9 @@ class MainApp(App):
     def on_start(self):
         
         for widofmainlist in self.widgets:
-            if type(widofmainlist) in (list, tuple, str):
-                self.freeze(widofmainlist)
+            if type(widofmainlist) in (list, tuple,  str):
+                wid = self.freeze(widofmainlist)
+                self.add_to_root(wid)
             else:
                 self.add_to_root(widofmainlist)
 
@@ -24,16 +25,17 @@ class MainApp(App):
         if type(wid) is list:
             parent = F.BoxLayout(orientation="horizontal")
             self.row_widget_adder(wid, parent)
-
+            return parent
         #tuple for floatlayout
         elif type(wid) is tuple:
             parent = F.FloatLayout()
             self.row_widget_adder(wid, parent) 
+            return parent
 
         elif type(wid) is str:
             widget = Builder.load_string(wid)
-            self.add_to_root(widget)
-
+            return widget
+        
             
 
     def add_to_root(self, widget):
@@ -48,13 +50,12 @@ class MainApp(App):
 
     def row_widget_adder(self, widofmainlist, parent):
         
+                
         for item in widofmainlist:
-            print(item)
             #put new boxlayout or floatlayout kwargs
             if type(item) is dict:
                 parent.__init__(**item)
-                print("dict")
             elif type(item) in (list, tuple, str):
-                self.freeze(item)
+                wid = self.freeze(item)
+                self.add_to_widget(wid, parent)
             else:self.add_to_widget(item, parent)
-        self.add_to_root(parent)        
