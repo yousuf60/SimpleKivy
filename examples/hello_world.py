3345888b276c31674e99cf6827875d9815451d21
yousuf <yosefmahmoud356@gmail.com>
2024-01-10 20:31:36 +0200

    examples

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -2,6 +2,5 @@ from simplekivy import SimpleKivy
 s = SimpleKivy(title="Hello world")
 
 s + [
-    s.Label(text="hello world!", color=(0,0,0,1)),
-    s.Button()
+    s.SLabel(text="hello world!", color=(0,0,0,1))
 ]
